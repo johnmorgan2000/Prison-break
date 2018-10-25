aada82ecff94fa844d6285634e367fa59a43b62b
John Morgan <40666115+johnmorgan2000@users.noreply.github.com>
2018-10-25 08:44:43 -0500

    Deleted javascript.js

--- javascript.js
+++
@@ -1,9 +0,0 @@
-class Player {
-    constructor(name, level, health, attack, defense) {
-        this.name = name;
-        this.level = level;
-        this.health = health;
-        this.attack = attack;
-        this.defense = this.defense;
-    }
-}
