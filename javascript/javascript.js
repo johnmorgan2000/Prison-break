4083da5232435a63d9775b081cc5ad80eb12bb29
basecamp <jmorgan@basecampcodingacademy.org>
2018-10-25 08:43:20 -0500

    Changed the trap class to an  Enemy class and made a function that allows it to move

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,16 +1,13 @@
 const gameWindow = document.querySelector("#game-window");
 
+// a class used to define the player
 class Player {
-    constructor(name, level, health, attack, defense, location) {
-        this.name = name;
+    constructor(level, location) {
         this.level = level;
-        this.health = health;
-        this.attack = attack;
-        this.defense = defense;
         this.location = location;
     }
 }
-
+// a class used to define objects the player will come across
 class Item {
     constructor(name, location) {
         this.name = name;
@@ -18,35 +15,59 @@ class Item {
     }
 }
 
+class Enemy extends Item {
+    constructor(name, location, moveDirection, spaces) {
+        super(name, location);
+        this.moveDirection = moveDirection;
+        this.spaces = spaces;
+    }
+}
+
+class Chest extends Item {
+    constructor(name, location, containsKey) {
+        super(name, location);
+        this.containsKey = containsKey;
+    }
+}
+
+//Defines how movement works for the player or possibly AI
 function move(event, objs) {
     if (event.keyCode == 38 && player.location.y != 0) {
-        player.location.y -= 50;
+        player.location.y -= 25;
         if (spaceOccupied(objs)) {
-            console.log("yes");
-            player.location.y += 50;
+            player.location.y += 25;
         }
-    } else if (event.keyCode == 39 && player.location.x != 450) {
-        player.location.x += 50;
+    } else if (event.keyCode == 39 && player.location.x != 575) {
+        player.location.x += 25;
         if (spaceOccupied(objs)) {
-            player.location.x -= 50;
+            player.location.x -= 25;
         }
-    } else if (event.keyCode == 40 && player.location.y != 450) {
-        player.location.y += 50;
+    } else if (event.keyCode == 40 && player.location.y != 575) {
+        player.location.y += 25;
         if (spaceOccupied(objs)) {
-            player.location.y -= 50;
+            player.location.y -= 25;
         }
     } else if (event.keyCode == 37 && player.location.x != 0) {
-        player.location.x -= 50;
+        player.location.x -= 25;
         if (spaceOccupied(objs)) {
-            player.location.x += 50;
+            player.location.x += 25;
         }
     }
 }
 
+// Determines if the player triggers game over
+function isDead(enemy) {
+    if (
+        enemy.location.y == player.location.y &&
+        enemy.location.x == player.location.x
+    ) {
+        return true;
+    }
+}
+
+// Determines if a space has an object on it
 function spaceOccupied(objs) {
     for (obj of objs) {
-        console.log(obj.location);
-        console.log(player.location);
         if (
             obj.location.y == player.location.y &&
             obj.location.x == player.location.x
@@ -55,53 +76,89 @@ function spaceOccupied(objs) {
         }
     }
 }
-
+// helper function used to generate walls quickly
 function generateWall(number, startCoordinates, direction, room, objectsList) {
     let pos = { x: startCoordinates.x, y: startCoordinates.y };
     for (i = 0; i < number; i++) {
         let wall = new Item("wall", { x: pos.x, y: pos.y });
         if (direction == "horizontal") {
-            pos.x += 50;
+            pos.x += 25;
         } else if (direction == "vertical") {
-            pos.y += 50;
+            pos.y += 25;
         }
         objectsList.push(wall);
         placeItem(wall, room);
     }
 }
 
+//places a list of items onto the screen
+function placeItems(objs, room) {
+    for (obj of objs) {
+        placeItem(obj, room);
+    }
+}
+
+// places an item onto the screen
 function placeItem(obj, room) {
     let item = document.createElement("div");
-    console.log(item);
     item.classList.add(obj.name);
     room.appendChild(item);
     item.style.top = obj.location.y + "px";
     item.style.left = obj.location.x + "px";
 }
 
-function startRoom() {
-    renderStartRoom(gameWindow);
-    let room = document.querySelector("#start-room");
+function moveGuard(enemy, room) {
+    let selector = "." + enemy.name;
+    let timer = 1;
+    let totalTrip = enemy.spaces * 2 + 1;
+    setInterval(() => {
+        if (timer == totalTrip) {
+            timer = 1;
+        } else if (timer > enemy.spaces) {
+            $(selector).remove();
+            if (enemy.moveDirection == "left") {
+                enemy.location.x += 25;
+            } else if (enemy.moveDirection == "right") {
+                enemy.location.x -= 25;
+            } else if (enemy.moveDirection == "up") {
+                enemy.location.y += 25;
+            } else if (enemy.moveDirection == "down") {
+                enemy.location.y -= 25;
+            }
 
-    let chest = new Item("chest", { x: 0, y: 50 });
-
-    placeItem(chest, room);
-    let objs = [chest];
-    generateWall(8, { x: 100, y: 100 }, "vertical", room, objs);
-    movePlayer(objs);
+            placeItem(enemy, room);
+            timer++;
+        } else if (timer <= enemy.spaces) {
+            $(selector).remove();
+            if (enemy.moveDirection == "left") {
+                enemy.location.x -= 25;
+            } else if (enemy.moveDirection == "right") {
+                enemy.location.x += 25;
+            } else if (enemy.moveDirection == "up") {
+                enemy.location.y -= 25;
+            } else if (enemy.moveDirection == "down") {
+                enemy.location.y += 25;
+            }
+            placeItem(enemy, room);
+            timer++;
+        }
+        if (isDead(enemy)) {
+            console.log("dead");
+        }
+    }, 800);
 }
+
+// puts in the startroomtemplate
 function renderStartRoom(win) {
     let source = document.querySelector("#start-room-template").innerHTML;
     win.innerHTML = source;
 }
 
-function movePlayer(objs) {
+//allows the player to move
+function playerAction(objs) {
     document.addEventListener("keydown", event => {
         move(event, objs);
         document.querySelector(".player").style.top = player.location.y + "px";
         document.querySelector(".player").style.left = player.location.x + "px";
     });
 }
-
-const player = new Player("John", 1, 100, 10, 10, { x: 0, y: 0 });
-startRoom();
